b26a908bf46e00ebf43ffe8d27641f538c1539c1
rossi_thomas <rossi.thomas@hotmail.com>
2026-09-08 22:35:09 +0800

    chore: 移动端工具栏标题更新为网罗音乐

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara";
+        dom.mobileToolbarTitle.textContent = "网罗音乐";
     }
 
     function updateMobileOverlayScrim() {
