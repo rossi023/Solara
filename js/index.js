f627e6142f08fdcf9af9eb0f9f35320acc6d6b92
rossi_thomas <rossi.thomas@hotmail.com>
2026-09-12 12:26:24 +0800

    feat: 新增酷我(kuwo)音源，可在菜单切换酷我音乐

--- js/index.js
+++ js/index.js
@@ -400,7 +400,8 @@ function preferHttpsUrl(url) {
 const SOURCE_OPTIONS = [
     { value: "netease", label: "网易云音乐" },
     { value: "joox", label: "JOOX音乐" },
-    { value: "kugou", label: "酷狗音乐" }
+    { value: "kugou", label: "酷狗音乐" },
+    { value: "kuwo", label: "酷我音乐" }
 ];
 
 function normalizeSource(value) {
